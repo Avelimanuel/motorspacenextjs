4c27ee30eace6f94093ba65dcb3de5a5fade3628
EmmanuelNdati <aveliemmanuel@gmail.com>
2024-12-29 00:07:28 +0300

    add vehicle server action

--- app/actions/addvehicle.js
+++ app/actions/addvehicle.js
@@ -1,7 +1,35 @@
-"use server"
-
+"use server";
+import connectToDatabase from "@/config/databaseconnection";
+import Vehicle from "@/models/Vehicle";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 async function addVehicle(formData) {
-    console.log(formData.get('model',))
+  await connectToDatabase();
+  const features = formData.getAll("features");
+  const images = formData
+    .getAll("images")
+    .filter((image) => image !== "") //filter any empty name
+    .map((image) => image.name); //get the image name
+
+  const vehicleData = {
+    make: formData.get("make"),
+    model: formData.get("model"),
+    yearOfManufacture: formData.get("yearOfManufacture"),
+    mileage: formData.get("mileage"),
+    engineSize: formData.get("engineSize"),
+    passengerCapacity: formData.get("passengerCapacity"),
+    transmissionType: formData.get("transmissionType"),
+    fuelType: formData.get("fuelType"),
+    color: formData.get("color"),
+    features: features, // Add collected features
+    isFeatured: formData.get("isFeatured") === "on", // Convert checkbox to boolean
+    images: images, // Collected image file names
+    description: formData.get("description"),
+  };
+  const newVehicle = new Vehicle(vehicleData)
+  await newVehicle.save()
+  revalidatePath('/','layout')
+  redirect(`/vehicles/${newVehicle._id}`)
 }
 
 export default addVehicle;
