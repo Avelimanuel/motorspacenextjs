4e21a85ad9b8b388150c4016de156526b910954e
EmmanuelNdati <aveliemmanuel@gmail.com>
2025-01-06 07:44:07 +0300

    vehicle added

--- app/actions/addvehicle.js
+++ app/actions/addvehicle.js
@@ -4,8 +4,10 @@ import Vehicle from "@/models/Vehicle";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import cloudinary from "@/config/cloudinary";
+
 async function addVehicle(formData) {
   await connectToDatabase();
+
   const features = formData.getAll("features");
   const images = formData.getAll("images").filter((image) => image !== "");
 
@@ -22,24 +24,30 @@ async function addVehicle(formData) {
     price: formData.get("price"),
     features: features, // Add collected features
     isFeatured: formData.get("isFeatured") === "on", // Convert checkbox to boolean
-
     description: formData.get("description"),
   };
 
   const imageUrls = [];
+
   for (let imageFile of images) {
     const imageBuffer = await imageFile.arrayBuffer();
     const imageArray = Array.from(new Uint8Array(imageBuffer));
     const imageData = Buffer.from(imageArray);
 
-    //convert to base 64
+    // Convert to base64
     const imageBase64 = imageData.toString("base64");
 
-    //make request to cloudinary
+    // Generate a unique public_id for the image
+    const uniqueId = `${vehicleData.make}-${
+      vehicleData.model
+    }-${Date.now()}-${Math.random().toString(36).substr(2, 9)}`;
+
+    // Upload image to Cloudinary
     const results = await cloudinary.uploader.upload(
       `data:image/png;base64,${imageBase64}`,
       {
         folder: "motorspace",
+        public_id: uniqueId, // Ensure uniqueness
       }
     );
     imageUrls.push(results.secure_url);
@@ -47,8 +55,11 @@ async function addVehicle(formData) {
 
   vehicleData.images = imageUrls;
 
+  // Save the vehicle to the database
   const newVehicle = new Vehicle(vehicleData);
   await newVehicle.save();
+
+  // Revalidate and redirect
   revalidatePath("/", "layout");
   redirect(`/vehicles/${newVehicle.make}-${newVehicle.model}`);
 }
