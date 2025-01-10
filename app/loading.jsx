fea072aee265f8b856dd8f41758517276915a8af
EmmanuelNdati <aveliemmanuel@gmail.com>
2025-01-10 07:51:55 +0300

    loading.js file

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,20 +1,30 @@
-"use-client";
+"use client"; // Add this to make it a client component
+
 import ClipLoader from "react-spinners/ClipLoader";
+import { useEffect, useState } from "react";
 
 const LoadingPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
   const override = {
     display: "block",
     margin: "100px auto",
-    borderColor: "#032b5e",
   };
-  return (
-    <ClipLoader
-      color="#032b5e"
-      cssOverride={override}
-      size={150}
-      aria-label="Loading spinner"
-    />
-  );
+
+  useEffect(() => {
+    // Set a timeout for 3 seconds (3000ms)
+    const timer = setTimeout(() => {
+      setIsLoading(false); // After 3 seconds, hide the loading page
+    }, 5000);
+
+    // Cleanup the timeout if the component unmounts before the timeout finishes
+    return () => clearTimeout(timer);
+  }, []);
+
+  // Render the loading page if isLoading is true
+  return isLoading ? (
+    <ClipLoader color="#3b82f6" cssOverride={override} size={150} />
+  ) : null;
 };
 
 export default LoadingPage;
--- app/vehicles/page.jsx
+++ app/vehicles/page.jsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import VehicleCard from "@/components/VehicleCard";
 import connectToDatabase from "@/config/databaseconnection";
 import Vehicle from "@/models/Vehicle";
+import VehicleSearch from "@/components/VehicleSearchForm";
 
 const VehiclesPage = async () => {
   await connectToDatabase();
@@ -9,6 +10,9 @@ const VehiclesPage = async () => {
 
   return (
     <section className="px-4 py-6">
+       <div className="max-width-7xl mx-auto flex flex-col items-start sm:px-6 lg:px-8">
+          <VehicleSearch />
+        </div>
       <div className="container-xl lg:container m-auto px-4 py-6">
         {vehicles.length === 0 ? (
           <h1>No vehicles found</h1>
