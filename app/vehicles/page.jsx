63a1f2c4e6d9806004447f35a596e06d776c176f
EmmanuelNdati <aveliemmanuel@gmail.com>
2025-01-11 23:12:41 +0300

    added pagination

--- app/vehicles/page.jsx
+++ app/vehicles/page.jsx
@@ -3,16 +3,22 @@ import VehicleCard from "@/components/VehicleCard";
 import connectToDatabase from "@/config/databaseconnection";
 import Vehicle from "@/models/Vehicle";
 import VehicleSearch from "@/components/VehicleSearchForm";
+import Pagination from "@/components/Pagination";
 
-const VehiclesPage = async () => {
+const VehiclesPage = async ({ searchParams }) => {
+  const { page = 1, pageSize = 9 } = await searchParams;
   await connectToDatabase();
-  const vehicles = await Vehicle.find({}).lean();
+  const skip = (page - 1) * pageSize;
+  const total = await Vehicle.countDocuments({});
+  const vehicles = await Vehicle.find({}).skip(skip).limit(pageSize);
+
+  const showPagination = total > pageSize;
 
   return (
     <section className="px-4 py-6">
-       <div className="max-width-7xl mx-auto flex flex-col items-start sm:px-6 lg:px-8">
-          <VehicleSearch />
-        </div>
+      <div className="max-width-7xl mx-auto flex flex-col items-start sm:px-6 lg:px-8">
+        <VehicleSearch />
+      </div>
       <div className="container-xl lg:container m-auto px-4 py-6">
         {vehicles.length === 0 ? (
           <h1>No vehicles found</h1>
@@ -22,6 +28,13 @@ const VehiclesPage = async () => {
               <VehicleCard key={vehicle._id} vehicle={vehicle} />
             ))}
           </div>
+        )}
+        {showPagination && (
+          <Pagination
+            page={parseInt(page)}
+            pageSize={parseInt(pageSize)}
+            totalItems={total}
+          />
         )}
       </div>
     </section>
