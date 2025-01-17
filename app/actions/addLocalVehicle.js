ef464dc446da06fa1ed6f5b4b71252954888f47e
EmmanuelNdati <aveliemmanuel@gmail.com>
2025-01-17 20:38:29 +0300

    added the locally used vehicle page

--- app/actions/addLocalVehicle.js
+++ app/actions/addLocalVehicle.js
@@ -1,29 +1,29 @@
-'use server'
-import connectToLocalVehicleDb from "@/config/localvehicledbconnection"
-import LocallyUsedVehicles from "@/models/LocallyUsed"
-import { revalidatePath } from "next/cache"
-import { redirect } from "next/navigation"
-import cloudinary from "@/config/cloudinary"
+"use server";
+import connectToLocalVehicleDb from "@/config/localvehicledbconnection";
+import LocallyUsedVehicles from "@/models/LocallyUsed";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import cloudinary from "@/config/cloudinary";
 
-async function AddLocalVehicle(formData){
-    await connectToLocalVehicleDb()
-    const features = formData.getAll("features")
-    const images = formData.getAll("images").filter((image)=>image !== "")
-    const localvehicleData = {
-        make: formData.get("make"),
-        model: formData.get("model"),
-        yearOfManufacture: formData.get("yearOfManufacture"),
-        mileage: formData.get("mileage"),
-        engineSize: formData.get("engineSize"),
-        passengerCapacity: formData.get("passengerCapacity"),
-        transmissionType: formData.get("transmissionType"),
-        fuelType: formData.get("fuelType"),
-        color: formData.get("color"),
-        price: formData.get("price"),
-        features: features, // Add collected features
-        isFeatured: formData.get("isFeatured") === "on", // Convert checkbox to boolean
-        description: formData.get("description"),
-      };
-      
+async function AddLocalVehicle(formData) {
+  //await connectToLocalVehicleDb()
+  const features = formData.getAll("features");
+  const images = formData.getAll("images").filter((image) => image !== "");
+  const localvehicleData = {
+    make: formData.get("make"),
+    model: formData.get("model"),
+    yearOfManufacture: formData.get("yearOfManufacture"),
+    mileage: formData.get("mileage"),
+    engineSize: formData.get("engineSize"),
+    passengerCapacity: formData.get("passengerCapacity"),
+    transmissionType: formData.get("transmissionType"),
+    fuelType: formData.get("fuelType"),
+    color: formData.get("color"),
+    price: formData.get("price"),
+    features: features, // Add collected features
+    isFeatured: formData.get("isFeatured") === "on", // Convert checkbox to boolean
+    description: formData.get("description"),
+  };
+  console.log(localvehicleData);
 }
 export default AddLocalVehicle;
--- app/locallyused/page.jsx
+++ app/locallyused/page.jsx
@@ -1,7 +1,30 @@
+import VehicleCard from "@/components/VehicleCard";
+import LocallyUsedVehicles from "@/models/LocallyUsed";
+import connectToLocalVehicleDb from "@/config/localvehicledbconnection";
+import VehicleSearch from "@/components/VehicleSearchForm";
 
-const LocallyUsed = () => {
- 
-    return ( <div>Locally used vehicles not available.We are currently working to restore...</div> );
-}
- 
+const LocallyUsed = async () => {
+  await connectToLocalVehicleDb();
+  const localvehicles = await LocallyUsedVehicles.find({});
+
+  return (
+    <section className="px-4 py-6">
+      <div className="max-width-7xl mx-auto flex flex-col items-start sm:px-6 lg:px-8">
+        <VehicleSearch />
+      </div>
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {localvehicles.length === 0 ? (
+          <h1 className="font-bold text-red-500 text-center">No Locally used vehicles found...we are curretlly working to restore</h1>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {localvehicles.map((localvehicle) => (
+              <VehicleCard key={localvehicle._id} vehicle={localvehicle} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
 export default LocallyUsed;
